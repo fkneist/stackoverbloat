f86be5b1b38469db1e206997ba3ec0c7bc58f176
Florian Kneist <fkneist@gmail.com>
2018-09-05 09:47:21 +0200

    add unix.stackexchange

--- stackoverbloat.user.js
+++ stackoverbloat.user.js
@@ -2,24 +2,25 @@
 // @name         stackoverbloat
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  distraction free stack exchange Q/A
+// @author       Florian Kneist
 // @match        https://stackoverflow.com/questions/*
 // @match        https://askubuntu.com/questions/*
 // @match        https://superuser.com/questions/*
+// @match        https://unix.stackexchange.com/questions/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    
+
     var avatars = document.getElementsByClassName('gravatar-wrapper-32')
     for(var i = 0; i < avatars.length; i++){
         console.log(avatars[i])
         avatars[i].innerHTML = "";
         avatars[i].style.backgroundColor = "#eff0f1"
     }
-    
+
     if(document.querySelector('#question-header > div > a')){
        document.querySelector('#question-header > div > a').style.visibility = 'hidden';
     }
@@ -35,22 +36,31 @@
     if (document.getElementById('sidebar')) {
         document.getElementById('sidebar').style.visibility = 'hidden';
     }
-    
+
+		if (document.getElementsByClassName('hero-container').length > 0) {
+				document.getElementsByClassName('hero-container')[0].style.height = '0px';
+		}
+
     // askubuntu begin
-		if(document.getElementsByClassName('s-hero')) {
-			document.getElementsByClassName('s-hero')[0].style.visibility = 'hidden';
+		if(document.getElementsByClassName('s-hero').length > 0) {
+				document.getElementsByClassName('s-hero')[0].style.height = '0px';
 		}
 
     if (document.getElementById('custom-header')) {
         document.getElementById('custom-header').style.visibility = 'hidden';
     }
-    
+
     if (document.getElementById('header')) {
         var header = document.getElementById('header')
         header.parentNode.removeChild(header)
     }
-   
+
     document.getElementById('content').style.fontFamily = 'Arial, Helvetica Neue, Helvetica, sans-serif'
     document.body.style.background = "white";
     // askubuntu end
+
+    // unix.stackexchange begin
+    document.body.style.backgroundImage = "url('')";
+    // unix.stackexchange end
+
 })();
